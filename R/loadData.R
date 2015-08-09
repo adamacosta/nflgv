f8aafccfc934e06a4690c08938fcf84ffdb4541c
Adam Acosta <acosta.adam@gmail.com>
2015-08-09 13:57:01 -0500

    Now parses the team colors and uses a left_join to return this with the cumulative margins.

--- R/loadData.R
+++ R/loadData.R
@@ -2,20 +2,29 @@
 #' 
 #' @importFrom dplyr transmute
 #' @importFrom dplyr select
+#' @importFrom dplyr left_join
 #' @importFrom magrittr %>%
 #' @export
 loadData <- function() {
-  filename <- system.file('extdata', 'nfl2014.csv', 
-                          package = 'nflgv')
-  nfl2014 <- read.csv(filename) %>%
+  pointFile <- system.file('extdata', 'nfl2014.csv', 
+                           package = 'nflgv')
+  colorFile <- system.file('extdata', 'nfl_team_colors.csv', 
+                           package = 'nflgv')
+  
+  nfl2014 <- read.csv(pointFile, stringsAsFactors = FALSE) %>%
     transmute(week = Week, winner = Winner, loser = Loser, 
+              # percent_margin prevents favoring high scores
               pts = percent_margin(PtsW, PtsL))
+  
+  colors <- read.csv(colorFile, stringsAsFactors = FALSE)
   
   movs <- aggregate(pts ~ winner + loser, 
                     data = select(nfl2014, winner, loser, pts),
                     mean)
   
-  teams <- aggregate(pts ~ winner, data = movs, sum)
+  teams <- aggregate(pts ~ winner, data = movs, sum) %>%
+    rename(team = winner) %>%
+    left_join(colors)
   
   return(list(movs = movs, teams = teams))
 }
